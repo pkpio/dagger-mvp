7b8fb4325650ad8f7e50d0c45fcd42eee0b50b5c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-06-07 01:40:21 +0200

    MainActivity cleanup

--- app/src/main/java/io/pkp/daggermvp/MainActivity.java
+++ app/src/main/java/io/pkp/daggermvp/MainActivity.java
@@ -1,14 +1,12 @@
 package io.pkp.daggermvp;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
